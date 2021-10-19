5a6e89a67dc903467050a09844c84a5ad7e66833
pappu-kumar <mpkpappu2001@gmail.com>
2021-10-19 12:49:07 +0530

    add cors

--- rootes/show.js
+++ rootes/show.js
@@ -5,23 +5,23 @@ const File = require('../models/file');
 router.get('/:uuid', async(req, res) => {
 
     try {
-        // featching file from data base
-        const file = await File.findOne({ uuid: req.params.uuid });
-        
-        if(!file) {
-            return res.render('download', { error: 'Link has been expired.' });    
-        }
+            // featching file from data base
+            const file = await File.findOne({ uuid: req.params.uuid });
+            
+            if(!file) {
+                return res.render('download', { error: 'Link has been expired.' });    
+            }
 
-        return res.render('download', {
-            uuid: file.uuid,
-            fileName: file.filename,
-            fileSize: file.size,
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-                // http://localhost:3000/files/download/hrhrf-hrh4r-jfbhv_ehjfhndbcdwu3
-        })
-    } catch(err) {
-        return res.render('download', { error: 'Something went wrong!' });
-    }
+            return res.render('download', {
+                uuid: file.uuid,
+                fileName: file.filename,
+                fileSize: file.size,
+                downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+                    // http://localhost:3000/files/download/hrhrf-hrh4r-jfbhv_ehjfhndbcdwu3
+            })
+        } catch(err) {
+            return res.render('download', { error: 'Something went wrong!' });
+        }
 
     
 });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-
-
 const app = express();
+const cors = require('cors');
+
 const PORT = process.env.PORT || 3000;
 // heroku website:    https://oxzip.herokuapp.com/ 
 
@@ -11,8 +11,15 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const connectDB = require('./config/db');
-
 connectDB();
+
+// Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    //  ['http://localhost:3000','http://localhost:5500']
+}
+
+app.use(cors(corsOptions));
 
 // Template engine
 app.set('views', path.join(__dirname, '/views'));
