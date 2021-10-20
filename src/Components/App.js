93eabbb8bbe3dd7cff752a73fe6d9a42d9431e4a
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-10-20 18:48:32 +0530

    Created till page one

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-// import Infobox from './Infobox';
+import Infobox from './Infobox';
 import Workarea from './Workarea';
 
 const App = () => {
+  const [route, setRoute] = useState('work');
+  console.log(route);
   return (
     <div className='container'>
-      <Header />
-      <Workarea />
+      <Header route={setRoute} />
+      {route === 'work' ? <Workarea /> : <Infobox />}
     </div>
   );
 };
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,41 @@
 import React from 'react';
-// import logo from '../img/X-Oval.png';
+import Workarea from './Workarea';
 
-const Header = () => {
-  return <div className='header'>Header</div>;
+import logo from '../img/X.png';
+import { RiProjectorLine } from 'react-icons/ri';
+import { RiAliensLine } from 'react-icons/ri';
+import { RiContrastLine } from 'react-icons/ri';
+
+const Header = (route) => {
+  console.log(route);
+  return (
+    <div className='header'>
+      <ul className='header__ul'>
+        <li class='header__li--icon'>
+          <img
+            src={logo}
+            className='header__li--icon-1'
+            alt='website logo ofr us'
+          />
+        </li>
+        <li className='header__li--main'>
+          <RiProjectorLine
+            className='header__li--main-1'
+            alt='Main'
+            onClick={() => {
+              route = 'main';
+            }}
+          />
+        </li>
+        <li class='header__li--aboutus'>
+          <RiAliensLine className='header__li--aboutus-1' />
+        </li>
+        <li class='header__li--theme'>
+          <RiContrastLine className='header__li--theme-1' />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
