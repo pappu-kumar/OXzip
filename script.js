5040bc174b99300d1cfc7836892d1604192ee31b
pappu-kumar <mpkpappu2001@gmail.com>
2021-10-24 13:15:00 +0530

    add script.js for deleteing file after 24hr

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ connectDB();
 
 // Get all records older than 24 hours 
 async function fetchData() {
-    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000)} })
+    
+    const pastData = new Date(Date.now() - 24*60*60*1000);
+
+    const files = await File.find({ createdAt : { $lt: pastData } });
+
     if(files.length) {
         for (const file of files) {
             try {
